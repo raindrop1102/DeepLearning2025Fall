8f781f7f08f9d6fb07b0ac5008887753e75e0b88
danny <christmas486929@gmail.com>
2025-10-02 01:00:58 +0800

    Add AlexNet VGG16

--- hw1/model.py
+++ hw1/model.py
@@ -50,3 +50,87 @@ class LetNet5(nn.Module):
         x = self.fc3(x)
         return x
         
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, kernel_size=5, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256*6*6, 4096),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Linear(4096, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+    
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(64, 64, 3, padding=1), nn.ReLU(inplace=False),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(128, 128, 3, padding=1), nn.ReLU(inplace=False),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(inplace=False),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(inplace=False),
+            nn.MaxPool2d(2, 2),
+        )
+    
+        self.classifier = nn.Sequential(
+            nn.Linear(512*7*7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+    
+if __name__ == "__main__":
+    model = AlexNet(num_classes=10)
+    print(model)
+
