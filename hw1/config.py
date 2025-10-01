41caae09002d92d5324c18b53c6c4b6a3f112715
danny <christmas486929@gmail.com>
2025-10-02 01:00:58 +0800

    Add AlexNet VGG16

--- hw1/config.py
+++ hw1/config.py
@@ -10,3 +10,21 @@ EPOCH = 100
 BATCH_SIZE = 32
 
 
+MODEL_WEIGHTS_PATH = Path('artifacts/vgg16_mnist.pth')
+IMAGE_SIZE = (224, 224)
+NORMALIZATION_MEAN = (0.1307, 0.1307, 0.1307)
+NORMALIZATION_STD = (0.3081, 0.3081, 0.3081)
+CLASS_NAMES = tuple(str(i) for i in range(10))
+
+# git pull -p
+# git merge danny
+# git push origin danny -f
+
+# git commit --amend
+
+# git status
+# git branch
+# git reset --soft 9e34e7c
+# git log --oneline
+# git reflog
+# git commit -m "garbage"
